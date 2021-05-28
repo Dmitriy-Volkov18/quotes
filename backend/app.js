953115bd8db24d1e9a4d79716f628e7e03e79847
Dmitriy Volkov <anexperienced.programmer@gmail.com>
2021-05-28 19:28:43 +0300

    commit

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const app = express()
+
+const cors = require("cors")
+require("dotenv").config()
+
+const quotesRoute = require("./routes/quoteRoute")
+
+const mongoConnection = require("./mongooseConnection")
+mongoConnection()
+
+app.use(express.json())
+app.use(cors())
+
+app.use("/api/quotes", quotesRoute)
+
+module.exports = app
+
+//dima Volk orgName angularApp
